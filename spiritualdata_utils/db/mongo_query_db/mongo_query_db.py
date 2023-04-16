206300732cfd63ea238fd2b4afba191650e97039
Shivam Ralli <shivamralli167@gmail.com>
2023-04-16 14:32:01 +0530

    FAD: Mongo query function

---
+++ spiritualdata_utils/db/mongo_query_db/mongo_query_db.py
@@ -0,0 +1,90 @@
+from spiritualdata_utils import init_logger
+from loguru import logger
+from pymongo import MongoClient, ReturnDocument
+
+init_logger()
+
+
+def mongo_query_db(
+    mongo_object: MongoClient,
+    query_type: str,
+    query: dict = None,
+    to_insert: dict = None,
+    collection: str = None,
+    projection: dict = None,
+):
+    """
+    The function to run various mongo queries for the spiritual_data codebase
+
+    Parameters
+    ----------
+    mongo_object : MongoClient
+        Mongo object that the queries need to run on, a db connection
+    query_type : str
+        Choices from find, find_one, insert, insert_one, update, update_one
+    query : dict, optional
+        The query to be run, by default None
+    to_insert : dict, optional
+        If something is to be inserted or updated, by default None
+    collection : str
+        the collection to query on, by default None
+    projection : dict, optional
+        The projection to apply to the query, by default None
+
+    Returns
+    -------
+    result : dict or None
+        The result of the query
+    """
+    logger.debug(
+        f"Running mongo_query_db with query_type: {query_type}, query: {query}, to_insert: {to_insert}, collection: {collection}, and projection: {projection}"
+    )
+    if collection is None:
+        raise ValueError("Collection name is required")
+
+    if query_type == "find":
+        result = mongo_object[collection].find(query, projection)
+        logger.debug(
+            f"Mongo query {query_type} on collection {collection} with query: {query} and projection: {projection}"
+        )
+        return [r for r in result]
+
+    elif query_type == "find_one":
+        result = mongo_object[collection].find_one(query, projection)
+        logger.debug(
+            f"Mongo query {query_type} on collection {collection} with query: {query} and projection: {projection}"
+        )
+        return result
+
+    elif query_type == "insert":
+        result = mongo_object[collection].insert(to_insert)
+        logger.debug(
+            f"Mongo query {query_type} on collection {collection} with to_insert: {to_insert}"
+        )
+        return result
+
+    elif query_type == "insert_one":
+        result = mongo_object[collection].insert_one(to_insert)
+        logger.debug(
+            f"Mongo query {query_type} on collection {collection} with to_insert: {to_insert}"
+        )
+        return result
+
+    elif query_type == "update":
+        result = mongo_object[collection].update(query, to_insert)
+        logger.debug(
+            f"Mongo query {query_type} on collection {collection} with query: {query} and to_insert: {to_insert}"
+        )
+        return result
+
+    elif query_type == "update_one":
+        result = mongo_object[collection].update_one(
+            query, to_insert, upsert=True
+        )
+        logger.debug(
+            f"Mongo query {query_type} on collection {collection} with query: {query} and to_insert: {to_insert}"
+        )
+        return result
+
+    else:
+        raise ValueError("Invalid query type")
---
+++ spiritualdata_utils/db/mongo_query_db/test_mongo_query_db.py
@@ -0,0 +1,79 @@
+import pytest
+from pymongo import MongoClient
+from spiritualdata_utils import init_logger
+from spiritualdata_utils import mongo_connect_db, mongo_query_db
+import os 
+
+
+@pytest.fixture(scope="module")
+def db():
+    # initialize the logger
+    init_logger()
+
+    # connect to the test database
+    mongo_uri = os.getenv("MONGO_URI")
+    database_name = os.getenv("MONGO_DB")
+    db = mongo_connect_db(uri=mongo_uri, database_name=database_name, refresh=True)
+
+    # define the test collection
+    collection_name = "test_collection"
+    collection = db[collection_name]
+
+    # insert some test data
+    data_to_insert = {"name": "John Doe", "age": 30}
+    inserted_id = mongo_query_db(
+        mongo_object=db,
+        query_type="insert_one",
+        to_insert=data_to_insert,
+        collection=collection_name,
+    )
+
+    yield db, collection, inserted_id, data_to_insert, collection_name
+
+    # drop the test collection
+    collection.drop()
+
+
+def test_find_data(db):
+    # retrieve the necessary variables from the fixture
+    db, collection, inserted_id, data_to_insert, collection_name = db
+
+    # check that the inserted ID is not None
+    assert inserted_id is not None
+
+    # check that the test data has been inserted
+    found_data = mongo_query_db(
+        mongo_object=db,
+        query_type="find_one",
+        query={"name": "John Doe"},
+        collection=collection_name,
+    )
+    assert found_data == data_to_insert
+
+
+def test_update_data(db):
+    # retrieve the necessary variables from the fixture
+    db, collection, inserted_id, data_to_insert, collection_name = db
+
+    # update the inserted data
+    updated_data = {"name": "Jane Doe", "age": 35}
+    result = mongo_query_db(
+        mongo_object=db,
+        query_type="update_one",
+        query={"name": "John Doe"},
+        to_insert={"$set": updated_data},
+        collection=collection_name,
+    )
+
+    # check that the update was successful
+    assert result.modified_count == 1
+
+    # check that the updated data is correct
+    found_data = mongo_query_db(
+        mongo_object=db,
+        query_type="find_one",
+        query={"name": "Jane Doe"},
+        projection = {'_id': 0},
+        collection=collection_name,
+    )
+    assert found_data == updated_data
